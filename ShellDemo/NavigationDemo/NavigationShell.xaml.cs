3f01bfe50e5b9de72f075525a10aebad8f397642
Daniel Hindrikes <daniel.hindrikes@cab.se>
2025-02-03 12:45:56 +0100

    demo starts here

--- ShellDemo/NavigationDemo/NavigationShell.xaml.cs
+++ ShellDemo/NavigationDemo/NavigationShell.xaml.cs
@@ -7,8 +7,5 @@ public partial class NavigationShell
     public NavigationShell()
     {
         InitializeComponent();
-
-        Routing.RegisterRoute("settings", typeof(SettingsPage));
-        Routing.RegisterRoute("details", typeof(DetailsPage));
     }
 }
